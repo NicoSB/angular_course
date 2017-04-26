ee3ddf7248d977010cd07ca13a4de1643b062441
NicoSB <ncstrbl@gmail.com>
2017-04-26 22:08:28 +0200

    finish module 20

--- prj-http-final/src/app/app.module.ts
+++ prj-http-final/src/app/app.module.ts
@@ -19,7 +19,9 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
-import { SiginComponent } from './auth/sigin/sigin.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,7 @@ import { SiginComponent } from './auth/sigin/sigin.component';
     RecipeStartComponent,
     RecipeEditComponent,
     SignupComponent,
-    SiginComponent
+    SigninComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +46,7 @@ import { SiginComponent } from './auth/sigin/sigin.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
